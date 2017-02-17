031a6f8162701fbf499b8e53fdd79afbe0d5c835
Nicolás <nirobayo@gmail.com>
2017-02-17 19:12:22 +0100

    - Implementado el movimiento, lo cual incluye pero no queda excluido al movimiento, el salto y el suelo.

---
+++ GameJamAA/Assets/Scripts/FloorCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag ("Player")) {
+			other.gameObject.GetComponent<CharacterMovement> ().onFloor = true;
+		}
+	}
+
+	void OnCollisionExit(Collision other){
+		if (other.gameObject.CompareTag ("Player")) {
+			other.gameObject.GetComponent<CharacterMovement> ().onFloor = false;
+		}
+	}
+
+}
