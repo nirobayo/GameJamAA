882b46f7a4c8a2fc75b4a02db0458b2c6ff27146
Cerociento <cerociento00@gmail.com>
2017-02-17 21:12:49 +0100

    Cerociento
    - Dispara!!!!

---
+++ GameJamAA/Assets/Script/Bala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour {
+
+	[SerializeField]
+	float velocidad = 10f;
+
+	void OnEnable () 
+	{
+		transform.GetComponent<Rigidbody> ().WakeUp ();
+		transform.GetComponent<Rigidbody> ().isKinematic = false;
+		transform.GetComponent<Rigidbody>().AddForce(Vector3.forward * velocidad ,ForceMode.Impulse);
+	}
+
+	void OnDisable()
+	{
+		transform.GetComponent<Rigidbody> ().Sleep ();
+		transform.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
+	void OnCollisionEnter(Collision hit)
+	{
+		gameObject.SetActive (false);
+		transform.position = transform.parent.position;
+	}
+
+
+}
---
+++ GameJamAA/Assets/Script/Disparo.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+
+	[SerializeField]
+	GameObject bala;
+	[SerializeField]
+	List<GameObject> cargador = new List<GameObject>();
+	[SerializeField]
+	int municion;
+
+	int _municion;
+
+	void Start()
+	{
+		GameObject _bala = Instantiate (bala, transform.GetChild(0).position, transform.rotation, gameObject.transform.GetChild(0));
+		cargador.Add (_bala);
+		cargador [0].SetActive (false);
+		_municion = municion;
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) 
+		{
+			if(_municion>0)
+			Disparando ();
+		}
+
+		if (Input.GetButtonDown ("Fire2"))
+		{
+			Recargando ();
+		}
+	}
+
+	GameObject Disparando()
+	{		
+		foreach (GameObject _balas in cargador) 
+		{
+			if (!_balas.activeSelf) 
+			{
+				_balas.SetActive (true);
+				_municion--;
+				return _balas;
+			}
+		}
+
+		GameObject nuevaBala = NuevaBala ();
+		nuevaBala.SetActive (true);
+		_municion--;
+        return NuevaBala ();
+
+	}
+
+	GameObject NuevaBala()
+	{
+		GameObject _bala = Instantiate (bala, transform.GetChild(0).position, transform.rotation, gameObject.transform.GetChild (0));
+		cargador.Add (_bala);
+		return _bala;
+	}
+
+	void Recargando()
+	{
+		_municion = municion;
+	}
+
+}
