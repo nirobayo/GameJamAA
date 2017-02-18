d11c5e1d1a8138088a910e3e443f8ec233844c06
Cerociento <cerociento00@gmail.com>
2017-02-18 14:50:28 +0100

    -Objetos del terreno añadidos

--- GameJamAA/Assets/Scripts/IA.cs
+++ GameJamAA/Assets/Scripts/IA.cs
@@ -10,24 +10,35 @@ public class IA : MonoBehaviour {
 	[SerializeField]
 	Transform[] patrulla;
 	int puntoRuta;
+	Animator anim;
+
+
 	void Start()
 	{
 		navMesh = GetComponent<NavMeshAgent> ();
 		Siguiente ();
+		anim = GetComponent<Animator> ();
 	}
 
 	void Update () 
 	{		
 		if (navMesh.remainingDistance < 0.25)
-			Siguiente ();
-			
+			Siguiente ();			
 	}
 		
 	void Siguiente()
 	{
 		if (patrulla.Length == 0)
 			return;
-		navMesh.SetDestination (patrulla[puntoRuta].position);
-		puntoRuta = (puntoRuta + 1) % patrulla.Length;
+		navMesh.SetDestination (patrulla[Random.Range(0,patrulla.Length)].position);
+		//puntoRuta = (puntoRuta + 1) % patrulla.Length;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			anim.SetTrigger ("CorreArma");
+		}
 	}
 }
