9ff9e63ce4c6215baa116f24d2b8f0bc34630c03
Cerociento <cerociento00@gmail.com>
2017-02-18 10:18:32 +0100

    Algo

---
+++ GameJamAA/Assets/Scripts/IA.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour {
+
+	NavMeshAgent navMesh;
+
+	[SerializeField]
+	Transform patrulla;
+
+	void Start()
+	{
+		navMesh = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () 
+	{
+		
+	}
+		
+}
---
+++ GameJamAA/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour {
+
+	[SerializeField]
+	GameObject CanvasPause;
+
+	bool pause;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+			pause = !pause;
+
+		if (pause)
+			PauseOn();
+		else
+			PauseOff();
+
+
+	}
+
+	public void PauseOn()
+	{
+		CanvasPause.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void PauseOff()
+	{
+		CanvasPause.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
