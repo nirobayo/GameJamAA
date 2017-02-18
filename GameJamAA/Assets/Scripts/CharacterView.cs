3f6c45b3d36bdab2f974157a6b5d238fbe87f7dd
Nicolás <nirobayo@gmail.com>
2017-02-18 13:23:10 +0100

    Ahora ve bien.

--- GameJamAA/Assets/Scripts/CharacterView.cs
+++ GameJamAA/Assets/Scripts/CharacterView.cs
@@ -7,7 +7,7 @@ public class CharacterView : MonoBehaviour {
 	[SerializeField] float lookSpeed;
 
 	Transform mainCamera;
-
+	Vector3 angle = new Vector3();
 	void Awake(){
 		mainCamera = Camera.main.transform;
 	}
@@ -16,18 +16,37 @@ public class CharacterView : MonoBehaviour {
 		float horizontal = Input.GetAxis ("Mouse X");
 		float vertical = Input.GetAxis ("Mouse Y");
 
-		transform.Rotate (0f,horizontal * lookSpeed,0f);
-		mainCamera.Rotate (-vertical * lookSpeed, 0f, 0f);
+		angle.x += -vertical * lookSpeed;
+		angle.y += horizontal * lookSpeed;
 
-		Vector3 angle = mainCamera.localEulerAngles;
 		if (angle.x < 300f && angle.x > 180f) {
+			angle.x = 300f;
+
+		} else if (angle.x > 60f && angle.x < 180f) {
+			angle.x = 60f;
+		}
+
+		//transform.Rotate (0f,horizontal * lookSpeed,0f);
+		//mainCamera.Rotate (, 0f, 0f);
+
+
+		//transform.Rotate (-vertical * lookSpeed,horizontal * lookSpeed,0f);
+		//Vector3 angle = mainCamera.localEulerAngles;
+
+
+		/*if (angle.x < 300f && angle.x > 180f) {
 			angle = new Vector3 (300f, 0f, 0f);
 
 		} else if (angle.x > 60f && angle.x < 180f) {
 			angle = new Vector3 (60f, 0f, 0f);
 		}
-			
 		mainCamera.localEulerAngles = angle;
+		*/
+
+	
+
+		transform.rotation = Quaternion.Euler(angle.x, angle.y, 0f);
+
 
 	}
 }
